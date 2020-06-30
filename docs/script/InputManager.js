fa4814b8dcc92f0b07bbe9126ca7dee973a865c2
Masato Yabe <y_masato0310@yahoo.co.jp>
2020-06-30 23:58:44 +0900

    InputManager：touch系のイベントは使わない方向で

--- docs/script/InputManager.js
+++ docs/script/InputManager.js
@@ -7,22 +7,11 @@ class InputManager
     {
         this.canvas = canvas;
 
-        canvas.addEventListener("touchstart", this.onTouchStartMethod, false);
         canvas.addEventListener("mousedown", this.onClickStartMethod, false);
-    }
-    onTouchStartMethod(eventInfo)
-    {
-        console.trace("Touch !");
-        var context = this.getContext("2d");
-        context.fillRect(eventInfo.clientX - 2, eventInfo.clientY - 2, 4, 4);
-        var touchDiv = document.getElementById("touch");
-        touchDiv.innerText = "Touch! : (" + eventInfo.clientX + ", " + eventInfo.clientY + ")";
     }
     onClickStartMethod(eventInfo)
     {
         console.trace("Click !");
-        var context = this.getContext("2d");
-        context.fillRect(eventInfo.clientX - 2, eventInfo.clientY - 2, 4, 4);
         var touchDiv = document.getElementById("click");
         touchDiv.innerText = "Click! : (" + eventInfo.clientX + ", " + eventInfo.clientY + ")";
     }
